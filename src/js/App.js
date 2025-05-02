0b09afa1344f90cb29e739dd37e55f9751e8d99f
Dominik <1706584+legovader09@users.noreply.github.com>
2025-05-02 13:08:26 +0100

    Update react template

---
+++ src/js/App.js
@@ -0,0 +1,5 @@
+﻿import React from 'react';
+
+const App = () => <div>App entry point.</div>;
+  
+export default App;
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from "./App";
 import '../scss/index.scss';
-
-const App = () => <div>App entry point.</div>;
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
